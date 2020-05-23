0520d2a5fa1f39fbd3cc69fbf1eb0dc4b45cfd16
Sam <sp990@uowmail.edu.au>
2020-05-23 21:00:29 +1000

    Added SearchBuilder and SearchBuilderTest

--- src/test/java/YoutubeSearchTest.java
+++ src/test/java/YoutubeSearchTest.java
@@ -1,4 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class YoutubeSearchTest {
 
+    @Test
+    public void searchBuilderTest(){
+
+        SearchBuilder sb = new SearchBuilder();
+
+        Map<String, String> param = new HashMap<>();
+        param.put("part", "snippet");
+        param.put("maxResult", "25");
+
+        String request = sb.searchBuilder(param);
+        System.out.println(request);
+        Assert.assertNotNull(request);
+    }
 }
