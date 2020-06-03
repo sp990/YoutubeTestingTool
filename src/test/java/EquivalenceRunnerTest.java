a295d7f38a7ffaf742091cbb3829d90dd0c98b1d
mw584 <65883938+mw584@users.noreply.github.com>
2020-06-04 08:41:07 +1000

    Update EquivalenceRunnerTest.java

--- src/test/java/EquivalenceRunnerTest.java
+++ src/test/java/EquivalenceRunnerTest.java
@@ -1,2 +1,24 @@
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EquivalenceRunnerTest {
+    @Test
+    public void test(){
+        Map<String, String> params = new HashMap<>();
+        params.put("part", "snippet");
+        params.put("channelId", "UC_x5XG1OV2P6uZZ5FSM9Ttw");
+        params.put("maxResults", "1");
+
+        try{
+            JSONObject result = YoutubeQuery.runQuery("playlists", params);
+            String title = result.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title").toString();
+            System.out.println(title);
+        }
+        catch(Exception e){
+
+        }
+    }
 }
