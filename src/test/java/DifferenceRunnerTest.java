932d12523f5ceb22ab2e29e9c90a5fc29432f85c
Sam <sp990@uowmail.edu.au>
2020-06-07 10:33:48 +1000

    Setting up DifferenceRunnerTest
    - New code is commented out

--- src/test/java/DifferenceRunnerTest.java
+++ src/test/java/DifferenceRunnerTest.java
@@ -1,2 +1,32 @@
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DifferenceRunnerTest {
+    /*
+    @Test
+    public void initialRequest_SendInitialRequest_ValidJsonObject() {
+        JSONObject initialRequest = initialRequest();
+        Assertions.assertNotNull(initialRequest);
+    }
+
+    @Test
+    public void extractTestFieldParameter_InputValidJSON_CorrectFieldReturned() throws IOException{
+        Map<String, String> params = new HashMap<>();
+        params.put("part", "snippet");
+        params.put("id", "UC_x5XG1OV2P6uZZ5FSM9Ttw");
+        JSONObject jsonObject = YoutubeQuery.runQuery("channel", params);
+    }
+
+    @Test
+    public void followUpRequest_SendFollowUpRequest_ValidJsonObject() {
+
+    }
+    */
 }
