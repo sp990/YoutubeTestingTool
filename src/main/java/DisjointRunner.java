7918088b42ecae14f65dbae7a2c2e309d5e0ab60
Hannah <hgr037@uowmail.edu.au>
2020-06-08 20:14:23 +1000

    tdd getters and setters
    WIP getters and setters tested

--- src/main/java/DisjointRunner.java
+++ src/main/java/DisjointRunner.java
@@ -1,4 +1,71 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+// I want to test the YouTube API that by querying for a channels uploaded videos I am getting only that channels videos.
+//to do this test I am using the disjoint metamorphic relation test.
+//I will get a channels uploads playlist and then get another random channels playlist and make sure the videosIDs are different.
+//therefore confirming i am only getting that users playlist
+
 public class DisjointRunner implements MetamorphicTestRunner{
+    private String sourceChannelID;
+    private String sourceUploadsID;
+    private ArrayList<String> sourcePlayList;
+    private String randomChannelID;
+    private String randomUploadsID = "";
+    private ArrayList<String> randomChannelUploadedPlayListItems = new ArrayList<>();
+
+    public void setRandomChannelID(){
+        randomChannelID = YoutubeRandomizer.getRandomChannelID();
+    }
+
+    public String getRandomChannelID(){
+        return randomChannelID;
+    }
+
+    public void setRandomChannelUploadsID(String randomChannelID) throws IOException {
+        Map<String,String> params = new HashMap<>();
+        params.put("part","contentDetails");
+        params.put("id",randomChannelID);
+        params.put("field","items/contentDetails/relatedPlaylists/uploads");
+
+        JSONObject result = YoutubeQuery.runQuery("channels", params);
+        JSONArray items = result.getJSONArray("items");
+        JSONObject jsonobject = items.getJSONObject(0);
+        JSONObject contentDetails = jsonobject.getJSONObject("contentDetails");
+        JSONObject relatedPlaylists = contentDetails.getJSONObject("relatedPlaylists");
+        randomUploadsID = relatedPlaylists.get("uploads").toString();
+    }
+
+    public String getRandomChannelUploadsID(){
+        return randomUploadsID;
+    }
+
+    public void setRandomChannelUploadedPlayListItems(String uploadsID) throws IOException {
+        Map<String,String> params = new HashMap<>();
+        params.put("part","contentDetails");
+        params.put("playlistId",uploadsID);
+        params.put("maxResults","25");
+
+        JSONObject result = YoutubeQuery.runQuery("playlistItems", params);
+        System.out.println(result);
+        JSONArray items = result.getJSONArray("items");
+        System.out.println(items);
+        for(int i = 0; i < items.length(); i++){
+            JSONObject jsonobject = items.getJSONObject(i);
+            JSONObject contentDetails = jsonobject.getJSONObject("contentDetails");
+            String videoID = contentDetails.get("videoId").toString();
+            randomChannelUploadedPlayListItems.add(videoID);
+        }
+    }
+
+    public ArrayList<String> getRandomChannelsUploadedPlayListItems(){
+        return randomChannelUploadedPlayListItems;
+    }
+
 
     @Override
     public void runTest() {
--- src/test/java/DisjointRunnerTest.java
+++ src/test/java/DisjointRunnerTest.java
@@ -2,22 +2,75 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DisjointRunnerTest {
     //init the system under test
     private final DisjointRunner disjointRunner = new DisjointRunner();
 
     @Test
-    public void getRandomChannelID_setStringtoanID_NotNull() {
-        String channelID = dijointRunner.getRandomChannelID();
-        AssertNotNull(channelID);
-
+    public void setRandomChannelID_setsRandomChannelID_NotNull() {
+        disjointRunner.setRandomChannelID();
+        Assertions.assertNotNull(disjointRunner.getRandomChannelID());
     }
-    //set up youtubequery class - test the class is initialized
 
-    //build query for get categories - test the query is rerutned as expected
+    @Test
+    public void getRandomChannelID_hasNotBeenSet_isNull(){
+        String emptyChannelID = disjointRunner.getRandomChannelID();
+        Assertions.assertNull(emptyChannelID);
+    }
 
-    //randomy select ccategory names - test the randomness
+    @Test
+    public void getRandomChannelID_hasBeenSet_isNotNull(){
+        disjointRunner.setRandomChannelID();
+        String initChannelID = disjointRunner.getRandomChannelID();
+        Assertions.assertNotNull(initChannelID);
+    }
+
+    @Test
+    public void setRandomChannelUploadsID_returnsAnUploadID_isNotNull() throws IOException {
+        disjointRunner.setRandomChannelUploadsID("UC_x5XG1OV2P6uZZ5FSM9Ttw");
+        Assertions.assertNotNull(disjointRunner.getRandomChannelUploadsID());
+    }
+
+    @Test
+    public void setRandomChannelUploadsID_returnsTheCorrectUploadID_equalsIDTest() throws IOException {
+        String IDTest = "UU_x5XG1OV2P6uZZ5FSM9Ttw";
+        disjointRunner.setRandomChannelUploadsID("UC_x5XG1OV2P6uZZ5FSM9Ttw"); //GoogleDevelopersID
+        Assertions.assertEquals(IDTest, disjointRunner.getRandomChannelUploadsID());
+    }
+
+    @Test
+    public void getRandomChannelUploadsID_hasBeenSet_isNotEmpty() throws IOException {
+        disjointRunner.setRandomChannelUploadsID("UC_x5XG1OV2P6uZZ5FSM9Ttw");
+        Assertions.assertNotEquals("",disjointRunner.getRandomChannelUploadsID());
+    }
+
+    @Test
+    public void getRandomChannelUploadsID_hasNotBeenSet_isEmpty(){
+        Assertions.assertEquals("",disjointRunner.getRandomChannelUploadsID());
+    }
+
+    @Test
+    public void setRandomChannelsUploadedPlayListItems_setsAListOfVideoIDs_isNotEmpty() throws IOException {
+        ArrayList<String> emptyList = new ArrayList<>();
+        disjointRunner.setRandomChannelUploadedPlayListItems("PLBCF2DAC6FFB574DE"); //googleDevelopers UploadsID
+        Assertions.assertNotEquals(emptyList, disjointRunner.getRandomChannelsUploadedPlayListItems());
+    }
+    @Test
+    public void setRandomChannelsUploadedPlayListItems_setsTheCorrectListOfVideoIDs_equalsTestList() throws IOException {
+        ArrayList<String> testList = new ArrayList<String>(Arrays.asList("GvgqDSnpRQM","fIc_VEQ7Vo0","V4DDt30Aat4","07718Vcwcyc","XDgC4FMftpg","OE63BYWdqC4","RQbmXxU2dkg",
+                "7nJdEXpvi1g", "7hakGJU9xco", "x9-F6dbCIHw", "gK7lUK0711E", "18TknKGC7tY", "CyRQJBBVI7g"));
+        disjointRunner.setRandomChannelUploadedPlayListItems("PLBCF2DAC6FFB574DE"); //googleDevelopers UploadsID
+        Assertions.assertEquals(testList, disjointRunner.getRandomChannelsUploadedPlayListItems());
+    }
+
+    @Test
+    public void getRandomChannelsUploadedPlayListItems_hasNotBeenSet_isEmpty(){
+        ArrayList<String> emptyList = new ArrayList<>();
+        Assertions.assertEquals(emptyList, disjointRunner.getRandomChannelsUploadedPlayListItems());
+    }
 
     //build a quesry for channels using that category and marked for kids - test the results are as expected
 
