cd43d0e5e05ccf2b8534d938832f7a3dec2afd7c
mw584 <65883938+mw584@users.noreply.github.com>
2020-06-08 11:54:01 +1000

    Update EquivalenceRunnerTest.java

--- src/test/java/EquivalenceRunnerTest.java
+++ src/test/java/EquivalenceRunnerTest.java
@@ -1,24 +1,37 @@
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EquivalenceRunnerTest {
     @Test
-    public void test(){
+    public void equivalenceTest1() throws IOException {
+
+        String channelId = YoutubeRandomizer.getRandomChannelId();
+
         Map<String, String> params = new HashMap<>();
         params.put("part", "snippet");
-        params.put("channelId", "UC_x5XG1OV2P6uZZ5FSM9Ttw");
-        params.put("maxResults", "1");
+        params.put("maxResults", "50");
+        params.put("fields", "items/id/playlistId,items/snippet(title,publishTime)");
+        params.put("channelId", channelId);
+        params.put("type", "playlist");
 
-        try{
-            JSONObject result = YoutubeQuery.runQuery("playlists", params);
-            String title = result.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title").toString();
-            System.out.println(title);
-        }
-        catch(Exception e){
+        JSONObject result = YoutubeQuery.runQuery("search", params);
+        System.out.println(result);
 
-        }
+        params.put("order", "date");
+        JSONObject followUp = YoutubeQuery.runQuery("search", params);
+        System.out.println(followUp);
+
+        List resultArray = result.getJSONArray("items").toList();
+        List followUpArray = followUp.getJSONArray("items").toList();
+
+        System.out.print(followUpArray.containsAll(resultArray) && resultArray.containsAll(followUpArray));
     }
 }
+//write test for non existent method, it fails, write actual method till it passes the test.
+//write test to get a channelID with results under 50. input bad channelID, make it fail, then put good channel ID.
+//have runtest method (main method) to be called run the test for equivalence.
