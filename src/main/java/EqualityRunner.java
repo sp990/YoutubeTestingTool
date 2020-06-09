3faac609d9e76fab8dfb78b48cf0e1a358ab99d2
Sam <sp990@uowmail.edu.au>
2020-06-10 09:08:19 +1000

    Added Equality Runner Implmentation

--- src/main/java/EqualityRunner.java
+++ src/main/java/EqualityRunner.java
@@ -1,7 +1,92 @@
-public class EqualityRunner implements MetamorphicTestRunner{
+
+import org.json.JSONObject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EqualityRunner implements MetamorphicTestRunner {
+
+    private boolean result;
+    private ArrayList<String> firstResultTitles = new ArrayList<>();
+    private ArrayList<String> secondResultTitles = new ArrayList<>();
+
+    //Equality test for testing tool
+    public boolean testEquality(JSONObject source, JSONObject other) throws IOException {
+
+
+        if(source.length() != other.length()){
+
+            return false;
+        }
+
+
+        for(int i = 0; i < source.getJSONArray("items").length(); i++) {
+
+            String title1 = (String) source.getJSONArray("items").getJSONObject(i).getJSONObject("snippet").get("title");
+            String title2 = (String) other.getJSONArray("items").getJSONObject(i).getJSONObject("snippet").get("title");
+            firstResultTitles.add(title1);
+            secondResultTitles.add(title2);
+
+
+            if(!title1.equals(title2)){
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //retrieves youtube api JSON
+    public void GenerateYoutubeJSON() throws IOException {
+
+        YoutubeDictionary yt1 = new YoutubeDictionary();
+        int r = (int) (Math.random() * yt1.size());
+
+        String search = yt1.get(r);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("part", "snippet");
+        params.put("maxResults", "5");
+        params.put("order", "rating");
+        params.put("q", search);
+        params.put("fields", "items/snippet/title");
+
+        System.out.println("Sending Source Request...");
+        JSONObject searchTest1 = YoutubeQuery.runQuery("search", params);
+        System.out.println("Sending Follow Up Request...");
+        JSONObject searchTest2 = YoutubeQuery.runQuery("search", params);
+
+        System.out.println("Comparing Results...");
+        //sets result
+        if (testEquality(searchTest1, searchTest2)) {
+
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+
+    //gets result
+    public boolean getResult(){
+        return result;
+    }
+
+    public void printReport() {
+        if(result)
+            System.out.println("Equality Test: Passed");
+        else
+            System.out.println("Equality Test: Failed");
+    }
 
     @Override
     public void runTest() {
-
+        System.out.println("---Starting Equality Test---");
+        try {
+            GenerateYoutubeJSON();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printReport();
     }
 }
--- src/test/java/EqualityRunnerTest.java
+++ src/test/java/EqualityRunnerTest.java
@@ -1,2 +1,106 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class EqualityRunnerTest {
+
+    EqualityRunner eqRun = new EqualityRunner();
+
+    //tests equality success from generate data methods - tdd test 2
+    @Test
+    public void testEqualitySuccess() throws IOException {
+
+        JSONObject sourceData = new JSONObject();
+        JSONObject passData = new JSONObject();
+        ArrayList<String> titles = new ArrayList<>();
+
+        sourceData = generateTestData();
+        passData = generatePassTestData();
+
+        String title = (String) sourceData.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title");
+        String title1 = (String) passData.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title");
+        Assertions.assertNotNull(title);
+        Assertions.assertFalse(title.isEmpty());
+        titles.add(title);
+        titles.add(title1);
+
+        Assertions.assertTrue(eqRun.testEquality(sourceData, passData));
+
+        for(int i = 0; i < titles.size(); i++){
+
+            System.out.println(titles.get(i));
+        }
+
+    }
+
+    //tests equality fail from generated data methods - tdd test 1
+    @Test
+    public void testEqualityFail() throws IOException {
+
+        JSONObject sourceData = new JSONObject();
+        JSONObject failData = new JSONObject();
+        ArrayList<String> titles = new ArrayList<>();
+
+        sourceData = generateTestData();
+        failData = generateFailTestData();
+
+        String title = (String) sourceData.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title");
+        String title1 = (String) failData.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").get("title");
+        Assertions.assertNotNull(title);
+        Assertions.assertFalse(title.isEmpty());
+        titles.add(title);
+        titles.add(title1);
+
+        Assertions.assertFalse(eqRun.testEquality(sourceData, failData));
+        for(int i = 0; i < titles.size(); i++){
+
+            System.out.println(titles.get(i));
+        };
+
+    }
+
+    //generates base test data for tdd test, contains title for comparing
+    public JSONObject generateTestData() {
+
+        JSONObject testData = new JSONObject();
+        testData.put("items", new JSONArray());
+        testData.getJSONArray("items").put(new JSONObject().put("snippet", new JSONObject()));
+        testData.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").put("title", "InitialTest");
+
+        return testData;
+    }
+
+    //generates a different title to assertfalse
+    public JSONObject generateFailTestData() {
+
+        JSONObject testDataFail = new JSONObject();
+        testDataFail.put("items", new JSONArray());
+        testDataFail.getJSONArray("items").put(new JSONObject().put("snippet", new JSONObject()));
+        testDataFail.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").put("title", "FailTest");
+
+        return testDataFail;
+    }
+
+    //generates the same title for asserttrue
+    public JSONObject generatePassTestData() {
+
+        JSONObject testDataPass = new JSONObject();
+        testDataPass.put("items", new JSONArray());
+        testDataPass.getJSONArray("items").put(new JSONObject().put("snippet", new JSONObject()));
+        testDataPass.getJSONArray("items").getJSONObject(0).getJSONObject("snippet").put("title", "InitialTest");
+
+        return testDataPass;
+    }
+
+    //runs program
+    @Test
+    public void test(){
+
+        eqRun.runTest();
+
+        System.out.println(eqRun.getResult());
+    }
 }
